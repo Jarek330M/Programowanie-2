47cc7e12d1ec25de9b463d084f93c5652ece7dbc
krys-jarek <szlachcian165@wp.pl>
2021-03-19 16:29:57 +0100

    działanie 2

---
+++ src/com/company/Dev2.java
@@ -0,0 +1,20 @@
+// Alt + insert
+package com.company;
+
+public class Dev2 {
+    //wymagania w zadaniu
+    private String author;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Dev2(String author) {
+        this.author = author;
+    }
+}
+
