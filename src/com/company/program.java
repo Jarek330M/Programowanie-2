33a3ce8838d4fb6b87e4f1fbc47fe9209120a010
krys-jarek <szlachcian165@wp.pl>
2021-03-19 16:29:57 +0100

    działanie 2

--- src/com/company/program.java
+++ src/com/company/program.java
@@ -5,5 +5,8 @@ public class program {
     public static void main(String[] args) {
 	// write your code here
         System.out.println(System.getProperty("java.version"));
+        Dev2 krystian = new Dev2("Krystian Strączek");
+        System.out.println(krystian.getAuthor());
+
     }
 }
